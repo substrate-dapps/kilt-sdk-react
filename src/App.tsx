56e27a3c8548b9d86167e6df3f938500b54a6a05
vadim.nicolai-EXT <vadim.nicolai-EXT@vitrifi.net>
2023-03-10 09:29:06 +0200

    Small improvement.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import * as Kilt from "@kiltprotocol/sdk-js";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [did, setDid] = useState("");
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  useEffect(() => {
+    const resolveWeb3Name = async () => {
+      const api = await Kilt.connect("wss://spiritnet.kilt.io");
+      const encodedDidDetails = await api.call.did.queryByWeb3Name("john_doe");
+
+      try {
+        const {
+          document: { uri },
+        } = Kilt.Did.linkedInfoFromChain(encodedDidDetails);
+        setDid(uri);
+      } catch {
+        setDid("unknown");
+      }
+    };
+    resolveWeb3Name();
+  });
+
+  return <div className="App">john_doe is {did}</div>;
 }
 
-export default App
+export default App;
